c048e89ebce406cd584d8e9e19ea18dcc91bb91a
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2022-08-01 19:48:23 -0700

    Remove old log function, package-lock.json.

--- packages/crate/src/api/util.ts
+++ packages/crate/src/api/util.ts
@@ -2,17 +2,15 @@ import Options from '../types/options'
 import observe from '../util/observe'
 
 export const enhancer =
-  (process.env.NODE_ENV === 'development' &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()) ||
-  undefined
+  (process.env.NODE_ENV === 'development' && (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()) || undefined
 
-export const log = (method: keyof Console, ...data) =>
-  console[method](
-    '%c<{ crate.js }>',
-    'font-weight: bold; font-style: italic',
-    ...data
-  )
+// NOTE: TypeScript refused to compile, even though this is valid JavaScript code to call a function by key.
+// export const log = (method: keyof Console, ...data) =>
+//   console[method](
+//     '%c<{ crate.js }>',
+//     'font-weight: bold; font-style: italic',
+//     ...data
+//   )
 
 export const observeOptions = (presets: Options, setter: Function) => {
   const store = {} as Options
